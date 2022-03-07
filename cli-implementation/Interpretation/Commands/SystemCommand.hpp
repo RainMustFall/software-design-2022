1967560ee74e69ec81dd104a3232651b5350e30b
Dzmitry Karshakevich <dim.korsh2011@yandex.ru>
2022-03-07 19:49:47 +0300

    Let system command read stdin

--- cli-implementation/Interpretation/Commands/SystemCommand.hpp
+++ cli-implementation/Interpretation/Commands/SystemCommand.hpp
@@ -16,45 +16,53 @@
  * Starts the specified child process
  * */
 class SystemCommand : public ICommand {
-public:
-  explicit SystemCommand(const std::string &command,
-                         const std::vector<std::string> &arguments)
-      : _command_line(AccumulateCommand(command, arguments)) {}
+ public:
+    explicit SystemCommand(const std::string& command,
+                           const std::vector<std::string>& arguments)
+        : _command_line(AccumulateCommand(command, arguments)) {}
 
-  IProcessPtr Execute(ExecutionContext &context) override {
-    auto process = std::make_shared<SynchronousProcess>();
-    TempFile stdoutFile;
-    TempFile stderrFile;
+    IProcessPtr Execute(ExecutionContext& context) override {
+        auto process = std::make_shared<SynchronousProcess>();
+        TempFile stdoutFile;
+        TempFile stderrFile;
+        TempFile stdinFile;
 
-    // XXX: std::system makes it impossible to collect program output normally,
-    // and other solutions require either using boost or using different
-    // system calls for *NIX and Windows
-    std::system(
-        (_command_line + ">" + stdoutFile.GetName() + " 2>" + stderrFile.GetName())
-            .c_str());
+        std::ofstream(stdinFile.GetName()) << context.GetStdin().rdbuf();
 
-    PrintFile(stdoutFile.GetName(), process->GetWritableStdout());
-    PrintFile(stderrFile.GetName(), process->GetWritableStderr());
-    return process;
-  }
+        // XXX: std::system makes it impossible to collect program output normally,
+        // and other solutions require either using boost or using different
+        // system calls for *NIX and Windows
+        int result = std::system(
+            (_command_line
+                + " >" + stdoutFile.GetName()
+                + " 2>" + stderrFile.GetName()
+                + " <" + stdinFile.GetName())
+                .c_str());
 
-private:
-  std::string _command_line;
-
-  static std::string
-  AccumulateCommand(const std::string &command,
-                    const std::vector<std::string> &arguments) {
-    std::string result = command + " ";
-    for (const auto &token : arguments) {
-      result += token + " ";
+        PrintFile(stdoutFile.GetName(), process->GetWritableStdout());
+        PrintFile(stderrFile.GetName(), process->GetWritableStderr());
+        process->SetReturnCode(
+            result == 0 ? ReturnCode::ok : ReturnCode::executionError);
+        return process;
     }
-    return result;
-  }
 
-  static void PrintFile(const std::string &filename, std::ostream &out) {
-    std::ifstream file(filename);
-    out << file.rdbuf();
-  }
+ private:
+    std::string _command_line;
+
+    static std::string
+    AccumulateCommand(const std::string& command,
+                      const std::vector<std::string>& arguments) {
+        std::string result = command + " ";
+        for (const auto& token: arguments) {
+            result += token + " ";
+        }
+        return result;
+    }
+
+    static void PrintFile(const std::string& filename, std::ostream& out) {
+        std::ifstream file(filename);
+        out << file.rdbuf();
+    }
 };
 
 #endif // CLI_IMPLEMENTATION_SYSTEMCOMMAND_HPP
