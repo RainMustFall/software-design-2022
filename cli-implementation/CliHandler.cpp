a09689e3da0c67b9d8e3702f6dc055e1a07037f6
Dima Korshakevich <dim.korsh2011@yandex.ru>
2022-03-07 23:52:27 +0300

    Add prompt for both systems

--- cli-implementation/CliHandler.cpp
+++ cli-implementation/CliHandler.cpp
@@ -1,14 +1,52 @@
 #include <sstream>
 
 #include "Parser.hpp"
+#include "Auxillary/CurrentDirectory.hpp"
 #include "DefaultConfiguration.hpp"
 #include "CliHandler.hpp"
 
 namespace cli {
 
+#ifdef _WIN32
+
+#include <windows.h>
+
+#define GREEN 10
+#define WHITE 7
+#define BLUE  3
+
+void PrintPrompt() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, GREEN);
+    std::cout << std::getenv("USERNAME");
+    SetConsoleTextAttribute(hConsole, WHITE);
+    std::cout << ":";
+    SetConsoleTextAttribute(hConsole, BLUE);
+    std::cout << CurrentDirectory();
+    SetConsoleTextAttribute(hConsole, WHITE);
+    std::cout << "$ " << std::flush;
+}
+
+#else
+
+#define BOLDGREEN   "\033[1m\033[32m"
+#define BOLDBLUE    "\033[1m\033[34m"
+#define RESET   "\033[0m"
+
+void PrintPrompt() {
+    std::cout
+        << BOLDGREEN << std::getenv("USERNAME")
+        << RESET << ":"
+        << BOLDBLUE << CurrentDirectory()
+        << RESET << "$ " << std::flush;
+}
+
+#endif
+
 void start_cli() {
     DefaultConfiguration configuration;
     std::string line;
+    PrintPrompt();
     std::getline(std::cin, line);
     while (line != "exit") {
         try {
@@ -22,6 +60,7 @@ void start_cli() {
             auto what = err.what();
             std::cerr << what << std::endl;
         }
+        PrintPrompt();
         std::getline(std::cin, line);
     }
 }
